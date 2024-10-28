2db2b3f47630663d507679bc25eb66a2967faa7f
yghaemi <yashar.ghaemi@antecedent.us>
2024-10-28 10:52:44 -0400

    commit

--- services/configure.py
+++ services/configure.py
@@ -5,7 +5,6 @@ import xml.etree.ElementTree as ET
 import time
 import subprocess
 import os
-
 import secrets
 import string
 
@@ -419,7 +418,7 @@ class ConfigAntecedent:
 
     def _set_nginx(self, step=1):
         destination = "./nginx-conf/ngconfig.conf"
-        source = "./webconfigs/step2.conf"
+        source = "./webconfigs/step3.conf"
         if step == 1:
             source = "./webconfigs/step1.conf"
         with open(source, "rb") as src_file:
